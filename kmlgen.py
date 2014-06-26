9bec01485f353e77f2b75c56fd504137b44b3a99
Juho Häppölä <juho.happola@iki.fi>
2014-06-26 14:20:59 +0200

    Animal Market on the right position, script help

--- kmlgen.py
+++ kmlgen.py
@@ -8,8 +8,14 @@ name = lambda foo: '    <name>%s</name>\n'%(foo,)
 desc = lambda foo: '    <description><![CDATA[<div dir=\"ltr\">%s</div>]]></description>\n    <styleUrl>#style12</styleUrl>\n'%(foo,)
 coord = lambda lon,lat: '    <Point>\n      <coordinates>%.7f,%.7f,%.7f</coordinates>\n    </Point>\n'%(lon,lat,0.0)
 
-kmlstring = '%s%s%s%s%s'%(head,name(sys.argv[1]),desc(sys.argv[2]),coord(float(sys.argv[3]),float(sys.argv[4])),tail)
+help = '\nA small helper script to generate a kml tag for a position.\nUsage:\n./kmlgen.py name description longitude latitude\n'
 
-print('add the following to the .kml file:\n\n')
-print(kmlstring)
+try:
+    kmlstring = '%s%s%s%s%s'%(head,name(sys.argv[1]),desc(sys.argv[2]),coord(float(sys.argv[3]),float(sys.argv[4])),tail)
+
+    print('\n\nadd the following to the .kml file:\n\n')
+    print(kmlstring)
+
+except IndexError:
+    print(help)
 
