8a8dea3d40c0898e6495b9b1b5d5bab59cc9a2a5
Juho Häppölä <juho.happola@iki.fi>
2014-06-26 14:48:05 +0200

    Added a list of souqs and restaurant
    Source: Raul Tempone, thank you

--- kmlgen.py
+++ kmlgen.py
@@ -7,11 +7,11 @@ tail = '</Placemark>\n'
 name = lambda foo: '    <name>%s</name>\n'%(foo,)
 desc = lambda foo: '    <description><![CDATA[<div dir=\"ltr\">%s</div>]]></description>\n    <styleUrl>#style12</styleUrl>\n'%(foo,)
 coord = lambda lon,lat: '    <Point>\n      <coordinates>%.7f,%.7f,%.7f</coordinates>\n    </Point>\n'%(lon,lat,0.0)
-
 help = '\nA small helper script to generate a kml tag for a position.\nUsage:\n./kmlgen.py name description longitude latitude\n'
+kmlfun = lambda foo,bar,baz,bat: '%s%s%s%s%s'%(head,name(foo),desc(bar),coord(baz,bat),tail)
 
 try:
-    kmlstring = '%s%s%s%s%s'%(head,name(sys.argv[1]),desc(sys.argv[2]),coord(float(sys.argv[3]),float(sys.argv[4])),tail)
+    kmlstring = kmlfun(sys.argv[1],sys.argv[2],float(sys.argv[3]),float(sys.argv[4]))
 
     print('\n\nadd the following to the .kml file:\n\n')
     print(kmlstring)
