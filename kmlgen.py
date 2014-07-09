847971b549123acd97a33dbbe84e7d5bdcf44147
Juho Häppölä <juho.happola@iki.fi>
2014-07-09 11:05:39 +0300

    colors and kmlgen

--- kmlgen.py
+++ kmlgen.py
@@ -5,7 +5,7 @@ import sys
 head = '<Placemark>\n'
 tail = '</Placemark>\n'
 name = lambda foo: '    <name>%s</name>\n'%(foo,)
-desc = lambda foo: '    <description><![CDATA[<div dir=\"ltr\">%s</div>]]></description>\n    <styleUrl>#style12</styleUrl>\n'%(foo,)
+desc = lambda foo: '    <description><![CDATA[<div dir=\"ltr\">%s</div>]]></description>\n    <styleUrl>#style684</styleUrl>\n'%(foo,)
 coord = lambda lon,lat: '    <Point>\n      <coordinates>%.7f,%.7f,%.7f</coordinates>\n    </Point>\n'%(lon,lat,0.0)
 help = '\nA small helper script to generate a kml tag for a position.\nUsage:\n./kmlgen.py name description longitude latitude\n'
 kmlfun = lambda foo,bar,baz,bat: '%s%s%s%s%s'%(head,name(foo),desc(bar),coord(baz,bat),tail)
